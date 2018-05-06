77f3645db9ef503974b3c51ce269b8409e02ad11
Michail Safronov <msaf1980@gmail.com>
2018-05-06 21:11:00 +0500

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import setup
 
 setup(name='gdbprint',
       version='0.1.0',
-      description='GDB data structura's browser',
-      url='http://github.com/msaf1980/funniest',
-      author='Flying Circus',
-      author_email='flyingcircus@example.com',
+      description='GDB data structuras browser',
+      url='http://github.com/msaf1980/gdbprint',
+      author='Michail Safronov',
+      author_email='msaf1980@gmail.com',
       license='MIT',
-      packages=['funniest'],
-      zip_safe=False)
-
+      packages=['gdbprint'],
+      zip_safe=True)
