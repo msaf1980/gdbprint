28b3155dd795b78ddb0f00cee011523f2772908c
Michail Safronov <msaf1980@gmail.com>
2019-07-30 09:24:48 +0500

    fix travis-ci

--- test.sh
+++ test.sh
@@ -1,7 +1,14 @@
-[ -z "${GDB_PYTHON}" ] && {
+#!/bin/sh
+
+GDB_PYTHON=`echo 'py import sys ; print(sys.executable)' | gdb -q | egrep '\(gdb\) /' | cut -d' ' -f2`
+
+[ -n "${GDB_PYTHON}" ] || {
     echo "incorrect GDB_PYTHON" >&2
     exit 1
 }
+
+#python -m pip install setuptools --user
+
 ${GDB_PYTHON} setup.py test || exit 1
 ${GDB_PYTHON} setup.py install --user || exit 1
 ${GDB_PYTHON} setup.py bdist || exit 1
